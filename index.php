81f87834ed88d0e0582efd345afdba51cff6e7ab
shwetasource <shwetasourceus@gmail.com>
2019-01-03 13:27:22 +0530

    initial setup

---
+++ index.php
@@ -0,0 +1,153 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="/css/style.css">
+  <!-- Bootstrap CSS -->
+  <link href="https://fonts.googleapis.com/css?family=Bree+Serif" rel="stylesheet">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
+    crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  
+  <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>
+<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'>
+
+  <title>Hello, world!</title>
+</head>
+
+<body>
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light" style="    background-color: #f8f9fa00 !important;">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup"
+      aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-item nav-link" href="#">Features</a>
+        <a class="nav-item nav-link" href="#">Pricing</a>
+        <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      </div>
+    </div>
+  </nav>
+  <!-- end of navbars -->
+  <div class="container-fluid main-img">
+    <div class="container main-qt">
+      <div class="qt">
+        <h1> THE <span class="new">NEW </span> </h1>
+        <h1>GENERATION </h1>
+        <h1>THINK ARE VERY <span class="new">CREATIVE! </span></h1>
+      </div>
+    </div>
+    <!-- end of main image -->
+  </div>
+  <!-- endof conatainer fluid -->
+
+  <div class="container cardsm ">
+    <div class="row">
+      <div class="cards">
+        <h1>Our Services</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo deleniti voluptate quia sed molestiae modi. Ex
+          totam incidunt expedita blanditiis possimus assumenda illum rem dolores.</p>
+      </div>
+      <div class="card1">
+        <i class="material-icons"> receipt </i>
+        <h3>Branding</h3>
+      </div>
+
+      <div class="card2">
+        <i class="material-icons">important_devices </i>
+        <h3>UI & UX</h3>
+        <br>
+        <br>
+        <i class="material-icons">devices_other </i>
+        <h3>Web Design</h3>
+      </div>
+      <div class="card3">
+        <i class="material-icons"> kitchen </i>
+        <h3>Hosting</h3>
+      </div>
+    </div> <!--end of row -->
+  </div>  <!--end of cards -->
+<div class="container">
+    <section class="client-slider-v1-wrapper section-padding">
+
+                    <div class="carousel slide" data-ride="carousel" id="client-slider-v1">
+                        <!-- Bottom Carousel Indicators -->
+                        <ol class="carousel-indicators">
+                            <li data-target="#client-slider-v1" data-slide-to="0" class="active"><img class="img-responsive " src="/img/a1.png" alt="">
+                            </li>
+                            <li data-target="#client-slider-v1" data-slide-to="1"><img class="img-responsive" src="/img/a2.png" alt="">
+                            </li>
+                            <li data-target="#client-slider-v1" data-slide-to="2"><img class="img-responsive" src="/img/a3.png" alt="">
+                            </li>
+                        </ol>
+
+                        <!-- Carousel Slides / Quotes -->
+                        <div class="carousel-inner text-center">
+<h2>Client's Testimonial</h2>
+                            <!-- Quote 1 -->
+                            <div class="item active">
+                           
+                                    <div class="row">
+                                        <div class="ravi testimonial-content">
+
+                                            <p> labore et dolore magna aliqua.n ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                            <span class="author">Kris Jegenes, <a href="#">Ventive</a></span>
+                                        </div>
+                                    </div>
+                                
+                            </div>
+                            <!-- Quote 2 -->
+                            <div class="item">
+                                <blockquote>
+                                    <div class="row">
+                                        <div class="ravi testimonial-content">
+
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod d exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                            <span class="author">Kris Jegenes, <a href="#">Ventive</a></span>
+                                        </div>
+                                    </div>
+                                </blockquote>
+                            </div>
+                            <!-- Quote 3 -->
+                            <div class="item">
+                                <blockquote>
+                                    <div class="row">
+                                        <div class="ravi testimonial-content">
+
+                                            <p> et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                            <span class="author">Kris Jegenes, <a href="#">Ventive</a></span>
+                                        </div>
+                                    </div>
+                                </blockquote>
+                            </div>
+                        </div>
+
+                        <!-- Carousel Buttons Next/Prev -->
+                        <a data-slide="prev" href="#client-slider-v1" class="left carousel-control"><i class="fa fa-chevron-left"></i></a>
+                        <a data-slide="next" href="#client-slider-v1" class="right carousel-control"><i class="fa fa-chevron-right"></i></a>
+                    </div>
+            
+        </div><!-- /.container -->
+    </section>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js'></script>
+    <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script>
+</div>
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut"
+    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
